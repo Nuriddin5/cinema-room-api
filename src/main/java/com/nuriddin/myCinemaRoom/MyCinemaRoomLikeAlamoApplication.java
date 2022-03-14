b01e18da2eb8ba9e746ced1ba6c6a51b6cbc2ab5
Nuriddin <nuriddinqurbonboyev@mail.ru>
2022-03-14 21:53:15 +0500

    Initial commit

---
+++ src/main/java/com/nuriddin/myCinemaRoom/MyCinemaRoomLikeAlamoApplication.java
@@ -0,0 +1,13 @@
+package com.nuriddin.myCinemaRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCinemaRoomLikeAlamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCinemaRoomLikeAlamoApplication.class, args);
+    }
+
+}
