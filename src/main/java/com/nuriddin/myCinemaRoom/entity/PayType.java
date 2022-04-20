b5a11a83ce419a7bda663339e574725b442ccfa7
Nuriddin <nuriddinqurbonboyev@mail.ru>
2022-04-20 17:40:02 +0500

    unknown

--- src/main/java/com/nuriddin/myCinemaRoom/entity/PayType.java
+++ src/main/java/com/nuriddin/myCinemaRoom/entity/PayType.java
@@ -3,15 +3,19 @@ package com.nuriddin.myCinemaRoom.entity;
 import com.nuriddin.myCinemaRoom.entity.template.AbsLongEntity;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 public class PayType extends AbsLongEntity {
@@ -22,5 +26,17 @@ public class PayType extends AbsLongEntity {
     @OneToOne
     Attachment logo;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        PayType payType = (PayType) o;
+        return getId() != null && Objects.equals(getId(), payType.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
     
