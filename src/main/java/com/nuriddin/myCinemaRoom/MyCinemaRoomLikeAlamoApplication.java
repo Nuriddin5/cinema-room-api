1d53d602c7a37ba5b11298dce247abd3879c33c9
Nuriddin5 <nuriddinqurbonboyev@mail.ru>
2022-10-24 00:33:30 +0500

    added static page

--- src/main/java/com/nuriddin/myCinemaRoom/MyCinemaRoomLikeAlamoApplication.java
+++ src/main/java/com/nuriddin/myCinemaRoom/MyCinemaRoomLikeAlamoApplication.java
@@ -3,16 +3,12 @@ package com.nuriddin.myCinemaRoom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableWebMvc
 // /swagger-ui/index.html#/ go to for swagger
-@EnableSwagger2
 public class MyCinemaRoomLikeAlamoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyCinemaRoomLikeAlamoApplication.class, args);
